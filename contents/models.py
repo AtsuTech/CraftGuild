a93c3023399c23497b77966b88b5f07a3714aa02
ahitotsui <atsushiabc1204@gmail.com>
2023-12-30 09:41:58 +0900

    コンテンツをカテゴリ別で表示する選択画面実装、他ページのデザイン調整

--- contents/models.py
+++ contents/models.py
@@ -2,7 +2,19 @@ from django.db import models
 from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel,MultiFieldPanel
+
+# ---タグ実装---
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase, Tag as TaggitTag
+# ---タグ実装---
+
+#　管理画面でタグとカテゴリ編集
+from wagtail.snippets.models import register_snippet
+
+
+
 
 
 #コンテンツの一覧のページ
@@ -78,5 +90,96 @@ class ContentsDetailPage(Page):
 
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
+
+        categories = TextCategory.objects
+        # for category in categories:
+        #     blogpages = TextPage.objects.filter(tags__name=category.name)
+        texts = TextPage.objects
+        #texts = TextPage.get_children(self)
+
+
+        #postsを"contents"という名前で子ページで取り扱うようにする
+        context["categories"] = categories
+        context["texts"] = texts
+        
         return context
 
+# テキストのカテゴリ
+@register_snippet
+class TextCategory(models.Model):
+    name = models.CharField(max_length=255)
+    panels = [
+        FieldPanel('name'),
+    ]
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = "カテゴリ"
+        verbose_name_plural = "categories"
+
+# テキストのタグ
+class TextPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'TextPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+
+@register_snippet
+class Tag(TaggitTag):
+    class Meta:
+        proxy = True
+
+# テキスト
+class TextPage(Page):
+
+    #　テキストのタイトル
+    text_name = models.CharField(
+        verbose_name="テキストのタイトル",
+        max_length=100,
+        blank=False,
+        null=False,
+    )
+
+    #　テキスト本文
+    body = RichTextField(
+        verbose_name="テキストの説明",
+        blank=True,
+        null=True,
+    )
+
+    # カバー画像
+    cover_image = models.ForeignKey(
+        "wagtailimages.Image",
+        verbose_name="カバー写真",
+        blank=True,
+        null=True,
+        related_name="+",
+        on_delete=models.SET_NULL,
+    )
+
+    #　タグ
+    tags = ClusterTaggableManager(
+        verbose_name="タグ",
+        through=TextPageTag, 
+        blank=True
+    )
+
+    #　カテゴリ
+    categories = ParentalManyToManyField(
+        'TextCategory',
+        verbose_name="カテゴリ",
+        blank=True
+        )
+
+    #管理画面で編集可能にするテーブルのカラム
+    content_panels = Page.content_panels + [
+        FieldPanel("text_name"),
+        FieldPanel("body"),
+        FieldPanel("cover_image"),
+        FieldPanel('tags'),
+        # MultiFieldPanel([
+        #     FieldPanel('tags'),
+        # ], heading="タグ"),
+        FieldPanel("categories"),
+    ]
